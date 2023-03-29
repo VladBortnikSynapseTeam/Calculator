0ca8ecd8b7b6562cbe01123652bf53f56ab9c862
Vlad <vladikjbortnik@gmail.com>
2023-03-29 16:25:33 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule
