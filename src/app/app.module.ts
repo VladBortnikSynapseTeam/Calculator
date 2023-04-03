1821d7964eed2c5cdee17505be7fd45fdf71deaa
Vlad <vladikjbortnik@gmail.com>
2023-04-03 12:53:04 +0300

    final fixes (ready to review)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainContainerComponent } from './components/main-container/main-container.component';
+import { DisplayComponent } from './components/display/display.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainContainerComponent
+    MainContainerComponent,
+    DisplayComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
